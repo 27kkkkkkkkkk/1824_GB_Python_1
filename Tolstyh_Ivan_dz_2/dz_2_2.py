f1207448fd6929ba26b72a8248c0b3bd28521552
Ivan <merazhuiki@hotmail.com>
2021-10-09 22:15:04 +0300

    Второе задание

--- Tolstyh_Ivan_dz_2/dz_2_2.py
+++ Tolstyh_Ivan_dz_2/dz_2_2.py
@@ -5,13 +5,15 @@ i = 0
 while i < len(agenda_list):
     if agenda_list[i].isdigit() or agenda_list[i][1:].isdigit():
        agenda_list[i] = agenda_list[i].zfill(2)
-       agenda_list.insert(i + 1, '"')
-       agenda_list.insert(i, '"')
+       agenda_list.insert(i + 1, ' "')
+       agenda_list.insert(i, '" ')
        i += 1
-       agenda_list[i] = agenda_list[i].replace('+5', ' +05', 1)
+       agenda_list[i] = agenda_list[i].replace('+5', '+05', 1)
     i += 1
-second_agenda_list = ' '.join(agenda_list)
-print(second_agenda_list)
+agenda_list = ' '.join(agenda_list)
+print(agenda_list.replace('  ', ''))
+
+
 
 
 
